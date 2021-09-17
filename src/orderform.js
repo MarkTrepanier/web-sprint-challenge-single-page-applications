08fdaacee324ceca30a8edd90871c79af1ef5878
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-17 18:01:45 -0400

    fixed text bug.

--- src/orderform.js
+++ src/orderform.js
@@ -2,29 +2,34 @@ import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import Axios from "axios";
 
+
+const FormStyle = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+.toppings, .personalInfo{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+`
+const initialOrder ={
+    name:'',
+    address:'',
+    size:'',
+    sauce:true,
+    cheese:true,
+    pepperoni:false,
+    anchovie:false,
+    onion:false,
+    mushroom:false,
+    specialInstructions:'',
+}
 export default function OrderForm(){
-    const FormStyle = styled.div`
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        .toppings, .personalInfo{
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-        }
-    `
-    const initialOrder ={
-        name:'',
-        address:'',
-        size:'',
-        sauce:true,
-        cheese:true,
-        pepperoni:false,
-        anchovie:false,
-        onion:false,
-        mushroom:false,
-        specialInstructions:'',
-    }
+
+
+
+
     const [order, setOrder]= useState(initialOrder);
 
     const inputOrder = (name, value) => {
@@ -33,25 +38,26 @@ export default function OrderForm(){
         })
     }
     const orderChange = evt => {
-        const {name, value, checked, type} = evt.target;
+        const {name, value, checked, type} = evt.target
         const valueToUse = type === 'checkbox' ? checked : value;
         inputOrder(name, valueToUse);
     }
+
     return(
         <FormStyle id='pizza-form'>
             <h1>Make Your Pizza!</h1>
             <div className='personalInfo'>
                 <label>{'Your Name '}
-                    <input name='name' value={order.name} type='text' id='name-input' key='name' />{/*todo: validation*/}
+                    <input name='name' value={order.name} type='text' id='name-input' key='name' onChange={orderChange} />{/*todo: validation*/}
                 </label>
                 <label>{'Your Address '}
-                    <input name='address' value={order.address} type='text' id='address-input'/>{/*todo: validation*/}
+                    <input name='address' value={order.address} type='text' id='address-input' onChange={orderChange}/>{/*todo: validation*/}
                 </label>
             </div>
             
             <div className='pizzaOptions'>
                 <label>{'Pizza Size '}
-                    <select name = 'size' value ={order.size} key='size' id='size-dropdown'>
+                    <select name = 'size' value ={order.size} key='size' id='size-dropdown' onChange={orderChange}>
                         <option value=''>-choose size-</option>
                         <option value='6"'>personal, 6"; +$2.99.</option>
                         <option value='10"'>small, 10"; +$4.99</option>
@@ -67,29 +73,28 @@ export default function OrderForm(){
                     <h2>Toppings</h2>
                     <h3>+ $0.75 each<span>(cheese and sauce $0.00)</span></h3>
                     <label>{'sauce '}
-                        <input name='sauce' value={order.sauce} key ={'0'} type='checkbox'/>
+                        <input name='sauce' checked={order.sauce} key ={'0'} type='checkbox' onChange={orderChange}/>
                     </label>
                     <label>{'cheese '}
-                        <input name='cheese' value={order.cheese} key ={'1'} type='checkbox'/>
+                        <input name='cheese' checked={order.cheese} key ={'1'} type='checkbox' onChange={orderChange}/>
                     </label>
                     <label>{'pepperoni '}
-                        <input name='pepperoni' value={order.pepperoni} key ={'2'} type='checkbox'/>
+                        <input name='pepperoni' checked={order.pepperoni} key ={'2'} type='checkbox' onChange={orderChange}/>
                     </label>
                     <label>{'anchovie '}
-                        <input name='anchovie' value={order.anchovie} key ={'3'} type='checkbox'/>
+                        <input name='anchovie' checked={order.anchovie} key ={'3'} type='checkbox' onChange={orderChange}/>
                     </label>
                     <label>{'onion '}
-                        <input name='onion' value={order.onion} key ={'4'} type='checkbox'/>
+                        <input name='onion' checked={order.onion} key ={'4'} type='checkbox' onChange={orderChange}/>
                     </label>
                     <label>{'mushroom '}
-                        <input name='mushroom' value={order.mushroom} key ={'5'} type='checkbox'/>
+                        <input name='mushroom' checked={order.mushroom} key ={'5'} type='checkbox' onChange={orderChange}/>
                     </label>
-                    
                 </div>
 
                 <div>
                     <label>special instructions
-                        <input name='specialInstructions' value={order.specialInstructions} type='text' id='special-text' key='ins'/>
+                        <input name='specialInstructions' value={order.specialInstructions} type='text' id='special-text' key='ins' onChange={orderChange}/>
                     </label>
                 </div>
 
