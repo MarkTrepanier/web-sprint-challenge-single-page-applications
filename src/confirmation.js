974a36ab00fea818163165b06f52e92536879b99
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-18 02:32:45 -0400

    requires submit disable and testing

--- src/confirmation.js
+++ src/confirmation.js
@@ -9,11 +9,15 @@ export default function Confirmation(props){
                 <div>
                     <h2>Thank you for your order!</h2>
                     <h3>{sentOrder.name}</h3>
+                    {sentOrder.address === '' ? <></>:
+                    <div>
+                        <h4>delivering to {sentOrder.address}</h4>
+                    </div>}
                     <h3>{sentOrder.adress}</h3>
                     <h4>one {sentOrder.size} pizza with {JSON.stringify(sentOrder.toppings)}</h4>
                     {sentOrder.specialInstructions === '' ? <></>:
                     <div>
-                        {sentOrder.specialInstructions}
+                        <h5>{sentOrder.specialInstructions}</h5>
                     </div>}
                 </div>
             }
